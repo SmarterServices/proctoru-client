c4f74244f81910d74cf22d1c5443439f7d15ff5d
Rafiul Islam <rafiul.islam@enosisbd.com>
2018-05-17 17:26:43 +0600

    #157547337 - Implement get schedule info

--- test/lib/client.js
+++ test/lib/client.js
@@ -110,5 +110,145 @@ describe('Client', function testClient() {
     });
   });
 
+  describe('Get Schedule Info Available Times List', function testClient() {
+
+    before('Create Mocker', function () {
+      proctorUMock.getEndpointMocker('getScheduleInfoAvailableTimesList');
+    });
+
+    const payload = mockData.getScheduleInfoAvailableTimesList.params;
+
+    it('Should list schedule info available times list', () => {
+      return client
+        .getScheduleInfoAvailableTimesList(payload)
+        .then((response)=>{
+          expect(response).to.eql(mockData.getScheduleInfoAvailableTimesList.response.valid);
+        });
+    });
+
+    it('Should fail for invalid [timeSent]', () => {
+
+      proctorUMock.removeInterceptor();
+      proctorUMock.getEndpointMocker('getScheduleInfoAvailableTimesList', 'timeOutError');
+
+      return client
+        .getScheduleInfoAvailableTimesList(payload)
+        .then(Promise.reject)
+        .catch((error)=>{
+          expect(error.response_code).to.eql(mockData.getScheduleInfoAvailableTimesList.response.timeOutError.response_code);
+          expect(error.message).to.eql(mockData.getScheduleInfoAvailableTimesList.response.timeOutError.message);
+        });
+    });
+
+    it('Should fail for missing [studentId]', () => {
+
+      const customPayload = Object.assign({},payload);
+      delete customPayload.studentId;
+
+      return client
+        .getScheduleInfoAvailableTimesList(customPayload)
+        .then(Promise.reject)
+        .catch((error)=>{
+          expect(error).to.eql('"studentId" is required');
+        });
+    });
+
+    it('Should fail for missing [timeZoneId]', () => {
+
+      const customPayload = Object.assign({},payload);
+      delete customPayload.timeZoneId;
+
+      return client
+        .getScheduleInfoAvailableTimesList(customPayload)
+        .then(Promise.reject)
+        .catch((error)=>{
+          expect(error).to.eql('"timeZoneId" is required');
+        });
+    });
+
+    it('Should fail for missing [examId]', () => {
+
+      const customPayload = Object.assign({},payload);
+      delete customPayload.examId;
+
+      return client
+        .getScheduleInfoAvailableTimesList(customPayload)
+        .then(Promise.reject)
+        .catch((error)=>{
+          expect(error).to.eql('"examId" is required');
+        });
+    });
+
+    it('Should fail for missing [startDate]', () => {
+
+      const customPayload = Object.assign({},payload);
+      delete customPayload.startDate;
+
+      return client
+        .getScheduleInfoAvailableTimesList(customPayload)
+        .then(Promise.reject)
+        .catch((error)=>{
+          expect(error).to.eql('"startDate" is required');
+        });
+    });
+
+    it('Should fail for missing [takeitnow]', () => {
+
+      const customPayload = Object.assign({},payload);
+      delete customPayload.takeitnow;
+
+      return client
+        .getScheduleInfoAvailableTimesList(customPayload)
+        .then(Promise.reject)
+        .catch((error)=>{
+          expect(error).to.eql('"takeitnow" is required');
+        });
+    });
+
+    it('Should fail for missing [duration]', () => {
+
+      const customPayload = Object.assign({},payload);
+      delete customPayload.duration;
+
+      return client
+        .getScheduleInfoAvailableTimesList(customPayload)
+        .then(Promise.reject)
+        .catch((error)=>{
+          expect(error).to.eql('"duration" is required');
+        });
+    });
+
+    it('Should fail for invalid [studentId]', () => {
+
+      const errorType = 'studentNotFoundError';
+
+      proctorUMock.removeInterceptor();
+      proctorUMock.getEndpointMocker('getScheduleInfoAvailableTimesList', errorType);
+
+      return client
+        .getScheduleInfoAvailableTimesList(payload)
+        .then(Promise.reject)
+        .catch((error)=>{
+          expect(error.response_code).to.eql(mockData.getScheduleInfoAvailableTimesList.response[errorType].response_code);
+          expect(error.message).to.eql(mockData.getScheduleInfoAvailableTimesList.response[errorType].message);
+        });
+    });
+
+    it('Should fail for invalid [examId]', () => {
+
+      const errorType = 'examNotFoundError';
+
+      proctorUMock.removeInterceptor();
+      proctorUMock.getEndpointMocker('getScheduleInfoAvailableTimesList', errorType);
+
+      return client
+        .getScheduleInfoAvailableTimesList(payload)
+        .then(Promise.reject)
+        .catch((error)=>{
+          expect(error.response_code).to.eql(mockData.getScheduleInfoAvailableTimesList.response[errorType].response_code);
+          expect(error.message).to.eql(mockData.getScheduleInfoAvailableTimesList.response[errorType].message);
+        });
+    });
+  });
 
 });
